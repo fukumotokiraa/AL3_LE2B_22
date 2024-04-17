712aa7b4a39d5656ff6abaa49ecb545dee6265cf
fukumotokiraa <122710kisose@gmail.com>
2024-04-17 17:01:22 +0900

    プロジェクト ファイルを追加します。

---
+++ scene/GameScene.cpp
@@ -0,0 +1,87 @@
+#include "GameScene.h"
+#include "TextureManager.h"
+#include <cassert>
+#include<ImGuiManager.h>
+
+GameScene::GameScene() {}
+
+GameScene::~GameScene() {}
+
+void GameScene::Initialize() {
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+	SpriteMario_ = TextureManager::Load("mario.jpg");
+	sprite_ = Sprite::Create(SpriteMario_, {100, 50});
+	ModelMario_ = TextureManager::Load("mario.jpg");
+	model_ = Model::Create();
+	worldTransform_.Initialize();
+	viewProjection_.Initialize();
+	SoundMokugyo_ = audio_->LoadWave("mokugyo.wav");
+	audio_->PlayWave(SoundMokugyo_);
+	VoiceMokugyo_ = audio_->PlayWave(SoundMokugyo_, true);
+}
+
+void GameScene::Update() {
+	Vector2 position = sprite_->GetPosition();
+	position.x += 2.0f;
+	position.y += 1.0f;
+	sprite_->SetPosition(position);
+	if (input_->TriggerKey(DIK_SPACE)) {
+		audio_->StopWave(VoiceMokugyo_);
+	}
+	ImGui::Begin("Debug1");
+	ImGui::Text("Kamata Tarou %d.%d.%d", 2050, 12, 31);
+	ImGui::InputFloat3("InputFloat3", inputFloat3);
+	ImGui::End();
+
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+	model_->Draw(worldTransform_, viewProjection_, ModelMario_);
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+	/*sprite_->Draw();*/
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
