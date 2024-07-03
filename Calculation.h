6199b2c97459b1006b7af7a6af6f380f1a668870
fukumotokiraa <122710kisose@gmail.com>
2024-07-03 16:32:20 +0900

    途中

--- Calculation.h
+++ Calculation.h
@@ -11,8 +11,6 @@ Vector3 Multiply(float scalar, const Vector3& v);
 
 float Length(const Vector3& v);
 
-Vector3 Scale(Vector3 v, float scalar);
-
 Vector3 Normalize(const Vector3& v);
 
 Vector3 TransformNormal(const Vector3& v, const Matrix4x4& m);
